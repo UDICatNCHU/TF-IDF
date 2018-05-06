697b13d54a561b47a4d0e700a6ded55e01216462
david <davidtnfsh@gmail.com>
2018-05-06 22:17:32 +0800

    update info

--- udicTfidf/management/commands/buildTfidf.py
+++ udicTfidf/management/commands/buildTfidf.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
         parser.add_argument('--lang', type=str)
 
     def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS('start building tfidf model!!!'))
         tfidf = TFIDF(options['lang'], uri)
-        logging.info('start build TF-IDF')
         tfidf.build()
         self.stdout.write(self.style.SUCCESS('build tfidf model success!!!'))
